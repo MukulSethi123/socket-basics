1ef62643553f163b8a90cddbc8d732801ce22e41
MukulSethi123 <mukul.ajay.1499@gmail.com>
2023-03-19 20:12:26 +0530

    basic chat app using socket.io, removed server as client acts as the server

--- client/index.js
+++ client/index.js
@@ -1,21 +1,33 @@
 const express = require("express");
-const io = require("socket.io-client");
+const http = require("http");
 const app = express();
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 //since the client and server are both on the same domain (localhost)
-const socket = io();
 
 const port = 3030;
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  socket.on("connet", () => {
-    console.log(socket.id);
-  });
   res.sendFile("/public/index.html", { root: __dirname });
 });
 
-app.listen(port, () => {
+// app.post("/", (req, res) => {
+//   console.log("hehe");
+//   res.send("hello");
+// });
+
+io.on("connection", (socket) => {
+  console.log("a user is connected");
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(port, () => {
   console.log("running client");
 });
